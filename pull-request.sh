ca5d94c546a47cb62c8905b406c83e0eaf7eddbd
Jens Schulze <jenschude@gmail.com>
2021-03-30 15:16:16 +0200

    remove draft for now

--- pull-request.sh
+++ pull-request.sh
@@ -68,7 +68,7 @@ create_pull_request() {
         # pull request already open
         echo "pull request from SOURCE ${SOURCE} to TARGET ${TARGET} is already open";
     else
-        PR_RESPONSE=$(gh pr create --repo ${GITHUB_REPOSITORY} --title "${TITLE}" --body "${BODY}" --base "${TARGET}" --head "${SOURCE}" --draft "${DRAFT}");
+        PR_RESPONSE=$(gh pr create --repo ${GITHUB_REPOSITORY} --title "${TITLE}" --body "${BODY}" --base "${TARGET}" --head "${SOURCE}");
         PR_NR=$(echo "${PR_RESPONSE}" | grep -E "^https://${GITHUB_REPOSITORY}/pull/[0-9]+$" | grep -oE "[0-9]+$")
         # handle_last_exit_code "$?"
 
